1999975432fb308d0da1371ac267bde03e5577bb
Jürgen 'tante' Geuter <tante@tante.cc>
2014-05-15 10:27:33 +0200

    Base functionality

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+    "bufio"
     "github.com/go-martini/martini"
     "github.com/martini-contrib/render"
+    "math/rand"
     "net/http"
+    "os"
+    "time"
 )
 
 func main() {
@@ -19,8 +23,36 @@ func main() {
 
     m.Post("/", func(r render.Render, req *http.Request) {
         req.ParseForm()
-        r.HTML(200, "greeting", req.PostForm.Get("name"))
+        quote := RandomQuote()
+
+        type QuoteInformation struct {
+            Quote string
+            Tech  string
+        }
+
+        info := QuoteInformation{quote, req.PostForm.Get("name")}
+
+        r.HTML(200, "greeting", info)
     })
 
     m.Run()
+}
+
+func Filereader(Filename string) []string {
+    file, _ := os.Open(Filename)
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines
+}
+
+func RandomQuote() string {
+    quotes := Filereader("quotes.txt")
+    // without seeding rand kinda sucks.
+    rand.Seed(time.Now().Unix())
+    return quotes[rand.Intn(len(quotes))]
 }
